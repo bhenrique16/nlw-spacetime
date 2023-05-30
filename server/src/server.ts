cbd9a9df4df2577378822f7afdf69cb1cc916d27
bhenrique16 <bh.henrique16@gmail.com>
2023-05-29 23:37:15 -0300

    Authentication

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 app.register(cors, {
   origin: true, // todas URLS de front end poderão acessar nosso backEnd
 })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
---
+++ web/src/app/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-gra-200 text-sm leading-relaxed">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://rocketseat.com.br"
+      >
+        {' '}
+        Rocketseat
+      </a>
+    </div>
+  )
+}
